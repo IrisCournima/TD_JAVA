a911b09e1e384ffdb1daf1919f21f1752607f719
IrisCournima <iris.cournima@gmail.com>
2015-12-22 16:44:35 +0100

    ajout fin lesson 2

--- InterventionJAVA/src/lanceur/lesson2.java
+++ InterventionJAVA/src/lanceur/lesson2.java
@@ -50,7 +50,7 @@ public class lesson2 {
 	 */
 	public static boolean est_premier(int n) {
 		
-		for (int i = 1; i < n; i++) {
+		for (int i = 1; i <= n; i++) {
 			if ((n%i==0)){
 				return false;
 			}else{
